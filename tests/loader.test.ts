29e341f50ff27b1d394a9f3566e454795b349f65
Jeff Wainwright <yowainwright@gmail.com>
2023-06-15 01:01:07 -0700

    feat: sets up loader for new tests

--- tests/loader.test.ts
+++ tests/loader.test.ts
@@ -1,89 +1,12 @@
-import * as global from "node:fs";
-import { join } from "node:path";
-import { vi, test, expect } from 'vitest'
-import { ImportMap } from "@jspm/import-map";
+import { vi, test, expect, describe, Mock } from 'vitest'
+import { parseUrlPkg } from "@jspm/generator";
 
 import {
-  constructPath,
-  constructLoaderConfig,
-  constructImportMap,
-  constructCachePath,
-  getLastPart,
-  getPackageNameVersionFromUrl,
-  getVersion,
-  parseModule,
-  ensureFileSync,
+  resolve,
 } from "../src/loader"
 
-vi.mock('url', () => ({
-  ...vi.importActual('url'),
-  fileURLToPath: (str: string) => `./${str}`,
-}));
-
-vi.mock('@jspm/import-map', () => {
-  const ImportMap = vi.fn()
-  return { ImportMap }
-})
-
-test('constructPath', () => {
-  const result = constructPath('foo')
-  expect(result).toStrictEqual('foo')
-});
-
-test('constructLoaderConfig', () => {
-  const result = constructLoaderConfig('/', 'file://bin')
-  expect(result).toStrictEqual({
-    cache: "file:/bin/.cache",
-    nodeImportMapPath: "file:/bin/node.importmap",
-    root: "./file://bin/",
-  })
-});
-
-test('constructImportMap', () => {
-  const path = `${process.cwd()}/tests/__fixtures__/fake.json`
-  constructImportMap(path, 'file://bin')
-  expect(ImportMap).toBeCalledWith({
-    rootUrl: "file://bin",
-    map: {
-      name: "is fake json"
-    },
-  })
-})
-
-test('constructCachePath', () => {
-  const result = constructCachePath({
-    cache: './bin',
-    modulePath: 'https://ga.jspm.io/npm:morgan@1.10.0/index.js',
-    debug: true,
+describe('resolve', () => {
+  test('start', () => {
+    expect(true).toBe(true)
   });
-  expect(result).toStrictEqual('bin/morgan@1.10.0/index.js')
-});
-
-test('getVersion', () => {
-  const urlParts = ['1.10.0/index.js']
-  const result = getVersion(urlParts)(0)
-  expect(result).toStrictEqual('1.10.0')
-});
-
-test('getLastPart', () => {
-  const result = getLastPart('1.10.0/index.js', '/')
-  expect(result).toStrictEqual('index.js')
-});
-
-test('getPackageNameVersionFromUrl', () => {
-  const result = getPackageNameVersionFromUrl('https://ga.jspm.io/npm:morgan@1.10.0/index.js')
-  expect(result).toStrictEqual({
-    file: 'index.js',
-    name: 'morgan',
-    version: '1.10.0',
-  });
-});
-
-
-test('should return the specifier if the module path is a node or file protocol', async () => {
-  const specifier = 'my-module';
-  const modulePath1 = 'node:fs';
-  const modulePath2 = 'file:///path/to/my/module.js';
-  expect(await parseModule(specifier, modulePath1)).toEqual(specifier);
-  expect(await parseModule(specifier, modulePath2)).toEqual(specifier);
 });

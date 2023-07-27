515fe4ac81cedfddfd14be3df4a659c9a09ecdc6
Jeff Wainwright <yowainwright@gmail.com>
2023-07-27 00:14:32 -0700

    fix: updates logger args type

--- src/logger.ts
+++ src/logger.ts
@@ -1,18 +1,18 @@
 import { LoggerOptions } from "./types";
 
 export const logger = ({ file, isLogging = false }: LoggerOptions) => ({
-  debug: (msg: string, args?: unknown) => {
+  debug: (msg: string, ...args: unknown[]) => {
     if (!isLogging) return;
-    if (args) console.debug(`jspm:[${file}]: ${msg}`, args);
+    if (args) console.debug(`jspm:[${file}]: ${msg}`, ...args);
     else console.debug(`jspm:[${file}]: ${msg}`);
   },
-  error: (msg: string, args?: unknown) => {
-    if (args) console.error(`jspm:[${file}]: ${msg}`, args);
+  error: (msg: string, ...args: unknown[]) => {
+    if (args) console.error(`jspm:[${file}]: ${msg}`, ...args);
     else console.error(`jspm:[${file}]: ${msg}`);
   },
-  info: (msg: string, args?: unknown) => {
+  info: (msg: string, ...args: unknown[]) => {
     if (!isLogging) return;
-    if (args) console.info(`jspm:[${file}]: ${msg}`, args);
+    if (args) console.info(`jspm:[${file}]: ${msg}`, ...args);
     else console.info(`jspm:[${file}]: ${msg}`);
   },
   setLogger: (isLogging = false) => logger({ file, isLogging }),
